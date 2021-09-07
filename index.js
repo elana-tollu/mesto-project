7f8f0bc1096ff590a166a5e88f74faa33065641c
Elana Tollu <elana.tollu@yandex.ru>
2021-09-07 18:50:58 +0300

    open-close popup add-item

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 function initButtons() {
     let buttonEdit = document.querySelector('.button-edit');  // Найти кнопку с карандашом
-    buttonEdit.addEventListener('click', editProfileShow);  // Прицепить на кнопку listener  // При клике на карандаш вызывается функция showEditProfile
+    buttonEdit.addEventListener('click', editProfileShow);  // Прицепить listener
 
     let editProfileForm = document.querySelector('#edit-profile');  // Найти форму редактирования профиля
     editProfileForm.addEventListener('submit', editProfileSave);  // Прицепить обработчик на submit
 
     let popupButtonCancel = document.querySelector('.popup__button-close');
     popupButtonCancel.addEventListener('click', editProfileCancel);
+
+
+    let buttonAdd = document.querySelector('.button-add');  // Найти кнопку с +
+    buttonAdd.addEventListener('click', showAddItem);  // Прицепить listener
+
+    let addItemForm = document.querySelector('#add-item');
+    addItemForm.addEventListener('submit', saveAddItem);
+
+    let addItemButtonCancel = document.querySelector('.popup__button-close_add-item');
+    addItemButtonCancel.addEventListener('click', cancelAddItem);
 }
 
 function editProfileShow() {
@@ -23,6 +33,19 @@ function editProfileSave(submitEvent) {
 
 function editProfileCancel() {
     let popup = document.querySelector('.popup');
+    popup.classList.remove('popup_opened');
+}
+
+
+function showAddItem() {
+    let popup = document.querySelector('.popup_add-item');  // Найти элемент popup
+    popup.classList.add('popup_opened');      // Присвоить модификатор элементу popup
+}
+
+function saveAddItem() {}
+
+function cancelAddItem() {
+    let popup = document.querySelector('.popup_add-item');
     popup.classList.remove('popup_opened');
 }
 
