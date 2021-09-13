50084dc93c5de3a93c38c77dfe11cbc78f03b395
Elana Tollu <elana.tollu@yandex.ru>
2021-09-13 22:41:07 +0300

    extract initial data

--- scripts/index.js
+++ scripts/index.js
@@ -27,33 +27,7 @@ function initButtons() {
 }
 
 function initContent() {
-    const initialCards = [
-        {
-          name: 'Архыз',
-          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-        },
-        {
-          name: 'Челябинская область',
-          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-        },
-        {
-          name: 'Иваново',
-          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-        },
-        {
-          name: 'Камчатка',
-          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-        },
-        {
-          name: 'Холмогорский район',
-          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-        },
-        {
-          name: 'Байкал',
-          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-        }
-        ];
-        initialCards.forEach(addItem);
+    initialCards.forEach(addItem);
 }
 
 function openPopup(popup) {
