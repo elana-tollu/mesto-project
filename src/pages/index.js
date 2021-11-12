2ea2025ff883bec1d1d08758e1add76c6ac67ee4
Elana Tollu <elana.tollu@yandex.ru>
2021-11-12 09:06:21 +0200

    move index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import './pages/index.css';
-import {initialCards} from './components/card.js';
-import {initAddItemForm, initAddItem, addItem} from './components/addItem.js';
-import {initEditProfile, initEditProfileForm} from './components/editProfile.js';
-import { initCards } from './components/card.js';
-import {initModal} from './components/modal.js';
-import {enableValidation} from './components/validate.js';
+import './index.css';
+import {initialCards} from '../components/card.js';
+import {initAddItemForm, initAddItem, addItem} from '../components/addItem.js';
+import {initEditProfile, initEditProfileForm} from '../components/editProfile.js';
+import { initCards } from '../components/card.js';
+import {initModal} from '../components/modal.js';
+import {enableValidation} from '../components/validate.js';
 
 function initComponents() { //подключение кнопок и мод.окон
 
