4738fa88c1b93e84262777f9d4ff2690b4313dd9
amvaino <ale5713@ya.ru>
2021-12-08 10:08:03 +0300

    fix: clear costants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,21 +1,8 @@
 export const objForm = {
-    formSelector: ".popup__form",
-    inputSelector: ".popup__form-field",
-    submitButtonSelector: ".popup__button-save",
-    inactiveButtonClass: "popup__button_disabled",
+    formSelector: '.popup__form',
+    inputSelector: '.popup__form-field',
+    submitButtonSelector: '.popup__button-save',
+    inactiveButtonClass: 'popup__button_disabled',
     errorClass: 'popup__input-error_active',
-    inputErrorClass: "popup__form-field_type_error",
+    inputErrorClass: 'popup__form-field_type_error',
 };
-
-
-/*
-
-    enableValidation({
-        formSelector: '.popup__form',
-        inputSelector: '.popup__form-field',
-        errorClass: 'popup__input-error_active',
-        inputErrorClass: 'popup__form-field_type_error',
-        submitButtonSelector: '.popup__button-save',
-    });
-
-*/
