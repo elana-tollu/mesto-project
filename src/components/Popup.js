5e4235e6c6b28c6586ae96bfeb4e81c7a7bf94ee
amvaino <ale5713@ya.ru>
2021-12-09 14:44:50 +0300

    metod removeEventListeners handleEscClose

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,21 +1,28 @@
 export class Popup {
     constructor(popupSelector, bodySelector) {
-      this._popup = document.querySelector(popupSelector);
-      this._body = document.querySelector(bodySelector);
+        this._popup = document.querySelector(popupSelector);
+        this._body = document.querySelector(bodySelector);
     }
 
-    open() { // открывает мод.окно
+    open() {
+        // открывает мод.окно
         this._popup.classList.add('popup_opened');
-        this._body.addEventListener('keydown', event => this._handleEscClose(event));
+        this._body.addEventListener('keydown', (event) =>
+            this._handleEscClose(event)
+        );
     }
 
-    close() { // закрывает мод.окно
+    close() {
+        // закрывает мод.окно
         this._popup.classList.remove('popup_opened');
-        this._body.removeEventListener('keydown', event => this._handleEscClose(event));
+        this._body.removeEventListener('keydown', (event) =>
+            this._handleEscClose(event)
+        );
+        this.removeEventListeners();
     }
 
     _handleEscClose(event) {
-        if(event.key === 'Escape') {
+        if (event.key === 'Escape') {
             this.close();
         }
     }
@@ -25,5 +32,8 @@ export class Popup {
         overlay.addEventListener('click', () => this.close());
         const buttonClose = this._popup.querySelector('.popup__button-close');
         buttonClose.addEventListener('click', () => this.close());
+    }
+    removeEventListeners() {
+        document.removeEventListener('keydown', this._handleEscClose);
     }
 }
